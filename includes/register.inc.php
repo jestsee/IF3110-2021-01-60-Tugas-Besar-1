7cc062068970fe60cf5002c4625654732dd2947b
Jesica <13519011@std.stei.itb.ac.id>
2021-10-20 11:15:20 +0700

    update register

--- includes/register.inc.php
+++ includes/register.inc.php
@@ -1,26 +1,49 @@
 <?php
     session_start();
-    include_once "register.php";
+    include_once "user.php";
 
-    //connect to db
+    // connect to db
     $pdo = new PDO('sqlite:user.db');
 
     if(isset($_POST["submit"])) {
-        $newusername = $_POST["usernamebaru"];
-        $newpassword = $_POST["passwordbaru"];
-        $newemail = $_POST["emailbaru"];
+        
+        $newusername = $_POST["username"];
+        $newpassword = $_POST["password"];
+        $newemail = $_POST["email"];
 
-        //cek username
-        //cek email 
-        //add user baru 
-        if((isset($_POST["usernamebaru"])) && (isset($_POST["passwordbaru"])) && (isset($_POST["emailbaru"]))){
-            addnewuser($newusername,$newpassword,$newemail);
+        // cek username
+        $result = isUsernameExist($newusername);
+        if($result === true) {
+            // username exist
+            header("location: ../register.php?error=usernameExist");
+            exit();
+        }
+
+        // cek email 
+        if(!filter_var($newemail, FILTER_VALIDATE_EMAIL)) {
+            header("location: ../register.php?error=invalidEmail");
+            exit();
+       }
+
+        // add user baru 
+        if((isset($_POST["username"])) && (isset($_POST["password"])) && (isset($_POST["email"]))){
+            
+            // masukin ke db
+            addUser($newemail, $newusername, $newpassword, 0);
+
+            // get id
+            $query = "SELECT * FROM user where username = '$newusername';";
+            $result = $db->query($query);
+            $row = $result->fetchArray();
+            
+            // create cookie
             $_SESSION['login'] = true;
             setcookie('id', $row['id'], time()+60,'/');
-            setcookie('key', hash('sha256', $usn), time()+60,'/');
+            setcookie('key', hash('sha256', $newusername), time()+60,'/');
             $_SESSION['level'] = 'user';
             header("location: ../index.php");
-        }else{
+            
+        } else {
             header("location: ../register.php");
         }
     }
--- register.php
+++ register.php
@@ -12,11 +12,11 @@
         <div class="split right">
             <div class="centered">
                 <h2><b>Register Account</b></h2><br>
-                <form action="register.inc.php" method="post">
+                <form action="includes/register.inc.php" method="post">
                     <input type="text" id='email' name='email' placeholder="Email" required>
                     <input type="text" id='username' name='username' placeholder='Username' required>
-                    <input type="text" id='password' name='password'placeholder='Password' required><br><br>
-                    <button type="submit" name="Daftar">Register</button>
+                    <input type="password" id='password' name='password'placeholder='Password' required><br><br>
+                    <button type="submit" name="submit">Register</button>
                 </form>
             </div>
         </div>
