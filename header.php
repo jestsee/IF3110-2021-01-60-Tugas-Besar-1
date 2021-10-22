41802b020327b94f019dc1ccc6d7c8f5cbc0425e
rheael <rpandumpi@gmail.com>
2021-10-22 13:18:29 +0700

    discard previous change

--- header.php
+++ header.php
@@ -37,7 +37,9 @@ function userHeader1($u_id) {
     echo "
     <div class='header'>
         <ul>
-            <li class='navitemleft'><a href=\"index.php\"><img src='css/logos2.jpg' width='150' height='50' style='padding: 0; margin: 0;'></a></li>
+            <li class='navitemleft'><a href=\"../index.php\"><img src='../css/logos2.jpg' width='150' height='50' style='padding: 0; margin: 0;'></a></li>
+            <form action='../search.php' method='post'><li class='navitemsearch'><input type='text' name='cari' placeholder='Search here :D'></li>
+            <li class='navitemsearch'><button type='submit' name='submit'>Search</button></li></form>
             <li class='navitem'>".$u_id."</li>
             <li class='navitem'><a href=\"../logout.php\">logout</a></li>
             <li class='navitem'><a href=\"../riwayat.php\">riwayat</a></li>
@@ -50,7 +52,9 @@ function adminHeader1($u_id) {
     echo "
     <div class='header'>
         <ul>
-            <li class='navitemleft'><a href=\"index.php\"><img src='css/logos2.jpg' width='150' height='50' style='padding: 0; margin: 0;'></a></li>
+            <li class='navitemleft'><a href=\"../index.php\"><img src='../css/logos2.jpg' width='150' height='50' style='padding: 0; margin: 0;'></a></li>
+            <form action='../search.php' method='post'><li class='navitemsearch'><input type='text' name='cari' placeholder='Search here :D'></li>
+            <li class='navitemsearch'><button type='submit' name='submit'>Search</button></li></form>
             <div class='kanan'>
             <li class='navitem'>".$u_id."</li>
             <li class='navitem'><a href=\"../logout.php\">logout</a></li>
